1d1af224278f00c9c730beb62fde9260d1d5dc51
justanuragmaurya <justanuragmaurya@gmail.com>
2025-07-21 23:56:27 +0530

    using packages and started generics

--- week-27/src/main.rs
+++ week-27/src/main.rs
@@ -42,38 +42,79 @@
 //     West
 // }
 
-// enum with value
-enum Shape{
-    Circle(f32),
-    Square(f32),
-    Reactangle(f32,f32)
-}
-
-fn main(){
-    // let direction = Direction::East;
-    // steer(direction);
-
-    let circle = Shape::Circle(10.0);
-    let sqaure = Shape::Square(10.0);
-    let rectangle = Shape::Reactangle(10.0,12.0);
-    get_area(&circle);
-    get_area(&sqaure);
-    get_area(&rectangle);    
-}
-
-// fn steer(dir:Direction){
-    //pattern matching
-    // match dir {
-    //     Direction::North=>{print!("Moved to North Direction")}
-    //     Direction::South=>{print!("Moved to South Direction")}
-    //     _ => { print!("Moved in Horizontal axis") } // Default Catch all block
-    // }
+// ----------------enum with value--------------
+// enum Shape{
+//     Circle(f32),
+//     Square(f32),
+//     Reactangle(f32,f32)
 // }
 
-fn get_area(shape : &Shape){
-    match shape{
-        Shape::Circle(radius)=>{ println!("{}",radius*radius*3.14)}
-        Shape::Square(side)=>{println!("{}",side*side)}
-        Shape::Reactangle(length, breadth)=>{println!("{}",length*breadth)}
+// fn main(){
+//     // let direction = Direction::East;
+//     // steer(direction);
+
+//     let circle = Shape::Circle(10.0);
+//     let sqaure = Shape::Square(10.0);
+//     let rectangle = Shape::Reactangle(10.0,12.0);
+//     get_area(&circle);
+//     get_area(&sqaure);
+//     get_area(&rectangle);    
+// }
+
+// // fn steer(dir:Direction){
+//     //pattern matching
+//     // match dir {
+//     //     Direction::North=>{print!("Moved to North Direction")}
+//     //     Direction::South=>{print!("Moved to South Direction")}
+//     //     _ => { print!("Moved in Horizontal axis") } // Default Catch all block
+//     // }
+// // }
+
+// fn get_area(shape : &Shape){
+//     match shape{
+//         Shape::Circle(radius)=>{ println!("{}",radius*radius*3.14)}
+//         Shape::Square(side)=>{println!("{}",side*side)}
+//         Shape::Reactangle(length, breadth)=>{println!("{}",length*breadth)}
+//     }
+// }
+
+// // - --- - - - - -- - - special enums like  result and option - -- - -- - -- - 
+// result ---- enum
+// use std::fs;
+
+// fn main(){
+//     let greeting_file_result = fs::read_to_string("hello.txt");
+    
+//     //returns a Ok or Err from the Result enum - > okay for success and err for errors;
+//     match greeting_file_result {
+//         Ok(file_content) => {
+//             println!("File read successfully: {:?}", file_content);
+//         },
+//         Err(error) => {
+//             println!("Failed to read file: {:?}", error);
+//         }q
+//     }
+// }
+
+// option ---- enum
+fn main(){
+    let s = String::from("Harkirat Singh"); 
+    let pos = find_first_a(&s);
+
+    match pos {
+        Some(position)=>{ println!("found at position : {}",position)}
+        None=>{println!("Not found !")}
     }
+
+}
+
+fn find_first_a(s : &String)->Option<i32>{
+    let mut count:i32 = 1;
+    for i in s.chars(){
+        if i == 'a' {
+            return Option::Some(count);
+        }
+        count += 1;
+    }
+    return Option::None;
 }
---
+++ week-28/src/main.rs
@@ -0,0 +1,47 @@
+// // -------------------- importing and using packages;   --------------------
+// use std::env;
+// use dotenv::dotenv;
+// use chrono::{Utc,Local};
+// fn main() {
+//     dotenv().ok();
+
+//     println!("Hello, world!");
+//     let name = env::var("NAME");
+//     let date = Utc::now();
+//     let ldate = Local::now();
+
+//     println!("{:?}\n{:?}",date,ldate);
+
+//     match name {
+//         Ok(namee)=>{println!("{}",namee)}
+//         Err(err)=>{println!("Error : {:?}",err)}
+//     }
+// }
+
+
+//--------------------Generics --------------------
+// the ugly way --- 
+// fn main(){
+//     println!("{}",add_f32(0.1, 10.8));
+//     println!("{}",add_u32(10, 90));
+    
+// }
+
+// fn add_u32(x:u32,y:u32)->u32{
+//     return x+y;
+// }
+
+// fn add_f32(x:f32,y:f32)->f32{
+//     return x+y;
+// }
+
+// the proper way or pretty way
+use std::ops::Add;
+fn main(){
+    println!("{}", add(10, 90)); 
+    println!("{}", add(10.1,90.6)); 
+}
+
+fn add<T:Add<Output = T>>(x:T,y:T)->T{
+    return x+y;
+}
